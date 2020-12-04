4228f8f1a3d08e91241d4a994672404cf3511275
unknown <pedro.coding.contact@gmail.com>
2020-12-04 16:33:29 -0300

    Faq

---
+++ src/components/FaqSection.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+
+const FaqSection = () =>{
+    return(
+            <div className="faq">
+                <h2>Any questions <span>FAQ</span></h2>
+                <div className="question">
+                    <h4>How to order</h4>
+                    <div className="awnser">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse laborum sed aut nobis, harum similique.
+                        </p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>How much do you charge</h4>
+                    <div className="awnser">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse laborum sed aut nobis, harum similique.
+                        </p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>How long does the process take</h4>
+                    <div className="awnser">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse laborum sed aut nobis, harum similique.
+                        </p>
+                    </div>
+                </div>
+                <div className="question">
+                    <h4>Do you sell prints</h4>
+                    <div className="awnser">
+                        <p>Lorem ipsum dolor sit amet.</p>
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Esse laborum sed aut nobis, harum similique.
+                        </p>
+                    </div>
+                </div>
+            </div>
+    )
+}
+
+export default FaqSection; 
